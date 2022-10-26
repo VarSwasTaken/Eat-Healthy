ff514fc39a03780f8520e196f21765d1c739f118
Maciek Sulima <miraxlesg@gmail.com>
2022-10-26 10:02:12 +0200

    first commit

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    account_created = db.Column(db.DateTime, default=datetime.utcnow())
+    avatar = db.Column(db.BLOB)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_photo(self, path):
+        filename = path
+        with open(filename, 'rb') as file:
+            self.avatar = file.read()
+   
+    def format_date(self):
+        string = self.account_created.strftime("%d-%m-%Y")
+        return string
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
