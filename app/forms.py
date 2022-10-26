8a8ad86266c2977877cbdd92d28b07299fb6de6a
Kacper Kustra <k.kustra100@gmail.com>
2022-10-26 10:31:13 +0200

    Fix typos

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from calendar import day_abbr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
@@ -11,59 +10,34 @@ class CreatePostForm(FlaskForm):
     url = StringField('URL', validators=[DataRequired()])
     post = PageDownField('Post content', validators=[DataRequired()])
     submit = SubmitField('add post')
-=======
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Regexp
-from app.models import User
->>>>>>> ff514fc39a03780f8520e196f21765d1c739f118
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(message="Username required"),
     ])
     password = PasswordField('Password', validators=[
-<<<<<<< HEAD
         DataRequired(message="Password required"),
     ])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-=======
-        DataRequired(message="Password required"), 
-    ])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
->>>>>>> ff514fc39a03780f8520e196f21765d1c739f118
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(message="Username required"),
-        Length(min=3, message="Username must be atleast 3 characters long")
+        Length(min=3, message="Username must be at least 3 characters long")
     ])
     email = StringField('Email', validators=[
-<<<<<<< HEAD
         DataRequired(message="Email required"),
         Email(message="Wrong email input")
     ])
     password = PasswordField('Password', validators=[
         DataRequired(message="Password required"),
-=======
-        DataRequired(message="Email required"), 
-        Email(message="Wrong email input")
-    ])
-    password = PasswordField('Password', validators=[
-        DataRequired(message="Password required"), 
->>>>>>> ff514fc39a03780f8520e196f21765d1c739f118
-        Length(min=8, message="Password must be atleast 8 characters long"),
-        Regexp('(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])', message="Password must contain atleast 1 lowercase, 1 uppercase & 1 digit")
+        Length(min=8, message="Password must be at least 8 characters long"),
+        Regexp('(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])', message="Password must contain at least 1 lowercase, 1 uppercase & 1 digit")
     ])
     password2 = PasswordField(
         'Repeat Password', validators=[
-<<<<<<< HEAD
             DataRequired(message="Field required"),
-=======
-            DataRequired(message="Field required"), 
->>>>>>> ff514fc39a03780f8520e196f21765d1c739f118
             EqualTo('password', message="Passwords must match")
     ])
     submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -98,7 +98,7 @@ def admin():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(current_user.username + '/post')
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -106,12 +106,12 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = current_user.username + '/post'
         return redirect(next_page)
-    return render_template('login.html', title="Sign In", form=form)
+    return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 def logout():
